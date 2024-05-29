ffeb7dfed6afd9fc98c0d38b38a09eb1bfe7e27d
christine <christine.mwangi@student.moringaschool.com>
2024-05-30 01:48:40 +0300

    ft: using api keys for a weather app

---
+++ index.js
@@ -0,0 +1,9 @@
+//use weather app Api
+const apiKey = 'YOUR_API_KEY';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
